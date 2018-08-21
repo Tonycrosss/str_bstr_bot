a3ffdb2774bcb962b776362002dd1393faf061ed
Anton S <starovoitov.anton@gmail.com>
2018-08-22 03:38:58 +0500

    убран авторизационный блок, надо токен подхватывать и грузиться с кукисами

--- bot.py
+++ bot.py
@@ -7,15 +7,23 @@ from selenium import common
 def run_bot():
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument("--mute-audio")
-    # chrome_options.add_argument("user-data-dir=C:\\Users\\Mpak\\AppData\\Local\\Google\\Chrome\\User Data\\Default")
+    chrome_options.add_argument("user-data-dir=C:\\Users\\Mpak\\AppData\\Local\\Google\\Chrome\\User Data\\Default")
     driver = webdriver.Chrome(chrome_options=chrome_options)
     driver.get('http://streambooster.ru/')
-    # Авторизуемся
-    login_button = driver.find_element_by_xpath("//a[text()='Авторизоваться']")
-    login_button.click()
-    twitch_log_button = driver.find_element_by_xpath("//a[@id='oauth-trigger']")
-    twitch_log_button.click()
+    # # Авторизуемся
+    # login_button = driver.find_element_by_xpath("//a[text()='Авторизоваться']")
+    # login_button.click()
+    # time.sleep(3)
+    # twitch_auth_button = driver.find_element_by_xpath("//a[@id='oauth-trigger']")
+    # twitch_auth_button.click()
 
+    # tw_username = driver.find_element_by_id("username")
+    # tw_username.send_keys("log")
+    # tw_pass = driver.find_element_by_xpath("//input[@name='password']")
+    # tw_pass.send_keys("pass")
+    # tw_log_button = driver.find_element_by_xpath("//span[@class='js-login-text']")
+    # tw_log_button.click()
+    # time.sleep(10)
 
 
 if __name__ == '__main__':
